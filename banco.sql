4cc061ef9f195db96c96e923c5e64d17804ba264
Lins S2 <118355678+linss22@users.noreply.github.com>
2024-12-17 07:05:22 -0400

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,4 +1,6 @@
-USE defaultdb;
+DROP DATABASE banco1022b;
+CREATE DATABASE banco1022b;
+USE banco1022b;
 CREATE TABLE IF NOT EXISTS produtos(
 	id BIGINT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50),
@@ -6,4 +8,4 @@ CREATE TABLE IF NOT EXISTS produtos(
     preco DECIMAL(10,2),
     imagem VARCHAR(300)
 );
-INSERT INTO produtos VALUES (1,'Iphone','Celular RUIM',5000.50,'SEM IMAGEM');
+INSERT INTO produtos VALUES (1,'Iphone','Celular RUIM',5000.50,'https://imgs.casasbahia.com.br/55067451/1g.jpg');
